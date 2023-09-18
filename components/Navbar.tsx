09d7da4dad98f3f29b0513c489e91375b1c5b21e
VarshaThottathilRejikumar <248777@ust.com>
2023-09-18 18:38:52 +0530

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Image from 'next/image'
+function Navbar() {
+  return (
+    <div className='bg-slate-800 p-2 md:p-1 border-b-1 md:border-b-0 border-slate-900 shadow-md text-center text-white'>
+  <h2 className='hover:bg-slate-950 p-2 md:p-1 rounded-md font-navfont'>ROUTE HUNTER</h2>
+    </div>
+
+  )
+}
+
+export default Navbar
